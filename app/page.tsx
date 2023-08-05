9d2b9aab652e493a79bbb9fc1b66f10ac213b512
Raul <51750327+mrexito@users.noreply.github.com>
2023-08-05 12:01:40 +0200

    3.5 Suspense

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className='text-3xl font-bold'>
-      Welcome Raul
+      Welcome Nextjs!
     </main>
   )
 }
